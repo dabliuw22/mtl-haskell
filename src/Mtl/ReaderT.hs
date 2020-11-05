98e2fbbf2b392c59b3d21c7c782ba54e3110f30f
Will Leyton <wleylop22@gmail.com>
2020-11-05 15:29:37 -0500

    Update

--- src/Mtl/ReaderT.hs
+++ src/Mtl/ReaderT.hs
@@ -7,12 +7,14 @@ import Control.Monad.Trans (liftIO)
 func1 :: MonadIO m => ReaderT String m String
 func1 = do
   env <- ask
+  _ <- liftIO $ print ("Func1: " <> env)
   res <- func2
   return ("Correlation: " ++ env ++ ", " ++ show res)
 
 func2 :: MonadIO m => ReaderT String m Int
 func2 = do
   env <- ask
+  _ <- liftIO $ print ("Func2: " <> env)
   let res = func3 env
   return res
 
