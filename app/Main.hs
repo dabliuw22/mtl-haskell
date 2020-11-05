de6e91c13b5f9113115060509129a77079f9792d
Will Leyton <wleylop22@gmail.com>
2020-11-05 14:52:28 -0500

    Update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified Monad.Reader as R
+import qualified Monad.State as S
 import qualified Monad.Writer as W
 import qualified Mtl.MaybeT as MT
 import qualified Mtl.ReaderT as RT
@@ -8,6 +9,9 @@ import qualified Mtl.WriterT as WT
 
 main :: IO ()
 main = do
+  R.run
+  S.run
+  W.run
   MT.run
-  WT.run
   RT.run
+  WT.run
